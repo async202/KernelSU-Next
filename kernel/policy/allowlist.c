db55a14f05ba85ccb28512c60d417a9e60d31f5e
async202 <96576912+async202@users.noreply.github.com>
2026-09-25 19:04:17 +0700

    kernel: allowlist: use __strscpy_pad for legacy kernels < 4.14.222

--- kernel/policy/allowlist.c
+++ kernel/policy/allowlist.c
@@ -495,7 +495,7 @@ static void migrate_profile(u32 version, struct app_profile *profile)
         if (profile->allow_su) {
             domain = profile->rp_config.profile.selinux_domain;
             if (strncmp(domain, "u:r:su:s0", domain_len) == 0) {
-                strscpy_pad(domain, KSU_DEFAULT_SELINUX_DOMAIN, domain_len);
+                __strscpy_pad(domain, KSU_DEFAULT_SELINUX_DOMAIN, domain_len);
                 pr_info("migrated domain of profile: %s\n", profile->key);
             }
         }
